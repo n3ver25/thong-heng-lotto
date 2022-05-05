178ea31892d78c292425081e983cfb333855c4ee
n3ver25 <98021448+n3ver25@users.noreply.github.com>
2022-05-06 04:20:02 +0700

    Update HomepageBanner.js

--- src/components/HomepageBanner.js
+++ src/components/HomepageBanner.js
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import logo from '../images/logo.jpg'
+import styled from 'styled-components'
 
-import homeBanner from '../images/home-banner.jpg'
+const ImageStyled = styled.img`
+  border-radius: 50%;
+`
 
 export const HomepageBanner = () => (
   <section className="homepage-banner">
-    <img src={logo} style={{ width: '200px' }} />
+    <ImageStyled src={logo} style={{ width: '200px' }} alt="ทองเฮงล๊อตโต้" />
   </section>
 )
