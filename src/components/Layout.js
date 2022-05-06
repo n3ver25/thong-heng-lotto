8e4a05cabcfc9ff0dbfc8722519043fb36fb9852
n3ver25 <art_sos_@hotmail.com>
2022-05-06 16:57:00 +0700

    fix seo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,7 @@ import './../styles/reset.css'
 import './../styles/common.css'
 import './../styles/style.css'
 
+
 export const Layout = ({ isHomepage, children, navigation }) => (
   <>
     <Header isHomepage={isHomepage} navigation={navigation} />
